ee15d241d1a835a688af9fc101cc636ff1d4a372
JB <thejibz@users.noreply.github.com>
2018-07-18 18:21:43 +0200

    remove operationId field for TypeName

--- src/swagger.js
+++ src/swagger.js
@@ -96,7 +96,7 @@ export const getAllEndPoints = (schema: SwaggerSchema, refs: RefType): {[string]
       const obj = route[method];
       console.log("plop" + JSON.stringify(obj, null, 2));
       const isMutation = ['post', 'put', 'patch', 'delete'].indexOf(method) !== -1;
-      const typeName = getGQLTypeNameFromURL(method, path) || obj.operationId;
+      const typeName = getGQLTypeNameFromURL(method, path);
       const parameterDetails = obj.parameters ? obj.parameters.map(param => getParamDetails(param, schema, refs)) : [];
       const endpoint: Endpoint = {
         parameters: parameterDetails,
