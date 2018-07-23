2f4a08b6dd2869bbe6c620e470b6f175ab654bee
JB <thejibz@users.noreply.github.com>
2018-07-23 17:52:35 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
 // @flow
-process.env.DEBUG = 'node-request-by-swagger'
-
 import type {GraphQLParameters, Endpoint, GraphQLType, RootGraphQLSchema, SwaggerToGraphQLOptions, GraphQLTypeMap} from './types';
 import rp from 'request-promise';
 import { GraphQLSchema, GraphQLObjectType } from 'graphql';
